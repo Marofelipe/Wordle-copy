4d98afe075486bfe121415e1e2911dfee53e7234
Carlos Eduardo Mancini Queiroz <carlos_mancini015@outlook.com>
2022-03-13 21:36:23 -0300

    Update Styles Keyboard

--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { TileContainerKeyboard } from "./styles";
 
-export default function Keyboard(props) 
+export default function Keyboard({...props}) 
 {
     return(
-        <div>
-            Keyboard
-        </div>
+        <TileContainerKeyboard {...props}>
+            <div>Marquin que desgraça é label seu filho da p</div>
+        </TileContainerKeyboard>
     )
 }
