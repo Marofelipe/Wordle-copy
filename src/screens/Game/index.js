760eccdf148a22b8184ee5af77be6c11b9f34452
Marcos Felipe <marofelipe@live.com>
2022-03-19 12:34:14 -0300

    useState implement

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import GameBoard from "../../components/GameBoard";
 import Keyboard from "../../components/Keyboard";
 import TopMenuElement from "../../components/TopMenu";
 
 
 export default function GameDashboard() {
-  const gameStatus = [
+  const [gameStatus, setGameStatus] = useState([
     {letters: ["B", "A", "I", "L", "E"] , status : ["right-place", "wrong", "wrong", "wrong", "wrong-place"]},
+    {letters: ["B", "A", "I", "L", "E"] , status : ["right-place", "right-place", "right-place", "right-place", "wrong-place"]},
     {letters: [] , status : []},
     {letters: [] , status : []},
     {letters: [] , status : []},
     {letters: [] , status : []},
-    {letters: [] , status : []},
-  ]
+  ]);
   return(
     <body>
       <TopMenuElement />
